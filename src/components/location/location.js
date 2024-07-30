43c4e7b3191c86ce0ab802a366e3df2dcdc546c2
FIT11 <FIT11@DESKTOP-GTEEPQQ>
2024-07-30 18:13:56 +0530

    create location interface initially

---
+++ src/components/location/location.js
@@ -0,0 +1,28 @@
+import { Grid, TextField, Typography } from '@mui/material';
+
+export default function Location() {
+    return (
+        <Grid container>
+            <Grid item={'true'} xs={3} sm={3} md={3} xl={3} lg={3}></Grid>
+            <Grid item={'true'} xs={6} sm={6} md={6} xl={6} lg={6}>
+                <Grid container spacing={2}>
+                    <Grid item xs={6} md={8}></Grid>
+                    <Grid item xs={6} md={4}></Grid>
+                    <Grid item xs={6} md={4} sm={4} xl={2} lg={2}>
+                        <Typography variant="subtitle1" gutterBottom>
+                            Location Code
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6} md={8} sm={8} xl={4} lg={4}>
+                        <TextField
+                            id="outlined-size-small"
+                            defaultValue="Small"
+                            size="small"
+                        />
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item={'true'} xs={3} sm={3} md={3} xl={3} lg={3}></Grid>
+        </Grid>
+    );
+}
